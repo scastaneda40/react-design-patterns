fee1015b6c32ff8c29167e92dc6a8f1780983db3
Stephen Castaneda <stephencastaneda@MacBookPro.lan>
2024-11-05 14:03:20 -0800

    list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import { SplitScreen } from './components/split-screen';
+import { RegularList } from './components/lists/Regular';
+import { SmallAuthorListItem } from './components/authors/SmallListItems';
+import { LargeAuthorListItem } from './components/authors/LargeListItems';
+import { authors } from './data/authors';
 
-const LeftSideComp = ({title}) => {
-  return (
-    <h2 style={{backgroundColor: 'crimson'}}>{title}</h2>
-  )
-}
 
-const RightSideComp = ({title}) => {
-  return (
-    <h2 style={{backgroundColor: 'burlywood'}}>{title}</h2>
-  )
-}
 function App() {
   return (
-  <SplitScreen leftWidth={1} rightWidth={3}>
-    <LeftSideComp title={'Left'}/>
-    <RightSideComp title={'Right'}/>
-  </SplitScreen>
+  <>
+  <RegularList items={authors} sourceName={"author"} ItemComponent={SmallAuthorListItem}/>
+  <RegularList items={authors} sourceName={"author"} ItemComponent={LargeAuthorListItem}/>
+  </>
   );
 }
 
