6c96d6b6e0caf516cf33632a13cbc6a2f2e3497a
Stephen Castaneda <stephencastaneda@MacBookPro.lan>
2024-11-06 11:38:34 -0800

    uncontrolled form

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import { RegularList } from './components/lists/Regular';
-import { SmallAuthorListItem } from './components/authors/SmallListItems';
-import { LargeAuthorListItem } from './components/authors/LargeListItems';
-import { authors } from './data/authors';
+import { UncontrolledForm } from "./components/uncontrolled-form";
 
 
 function App() {
   return (
-  <>
-  <RegularList items={authors} sourceName={"author"} ItemComponent={SmallAuthorListItem}/>
-  <RegularList items={authors} sourceName={"author"} ItemComponent={LargeAuthorListItem}/>
-  </>
+    <>
+      <UncontrolledForm />
+    </>
   );
 }
 
